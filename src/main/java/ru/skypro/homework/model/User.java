bb382cb36ceb890176f9db1ca0965984a2ec6e5a
Stupendos <topakovdenis671@gmail.com>
2025-04-16 22:59:55 +0700

    asd

---
+++ src/main/java/ru/skypro/homework/model/User.java
@@ -0,0 +1,30 @@
+package ru.skypro.homework.model;
+
+import ru.skypro.homework.dto.Role;
+
+import javax.persistence.*;
+import java.util.UUID;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+    @Column(name = "user_name", nullable = false)
+    private String username;
+    @Column(name = "password", nullable = false)
+    @Size(min = 8, message = "Пароль должен содержать не менее 8 символов")
+    private String password;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "phone", nullable = false)
+    private String phone;
+    @Column(name = "role")
+    private Role role;
+}
