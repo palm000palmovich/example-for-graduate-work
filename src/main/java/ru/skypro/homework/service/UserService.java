0130baab82a8ba3d2278d2b67bea4a10ae8f8715
IrinaSerebryakova <ia.serebr@gmail.com>
2025-04-19 12:37:18 +0300

    feat (CommentController, UserController):
    Созданы сущности Comment, Comments, CreateOrUpdateComment,User, UpdateUser.

--- src/main/java/ru/skypro/homework/service/UserService.java
+++ src/main/java/ru/skypro/homework/service/UserService.java
@@ -1,13 +1,5 @@
 package ru.skypro.homework.service;
 
-import ru.skypro.homework.dto.User;
-
-import java.util.Map;
-
 public interface UserService {
-    Map<String, String> setPassword(String newPassword);
 
-    Map<String, String> getInfoAboutCurrentUser();
-
-    User initFirstNameAndLastName();
 }
