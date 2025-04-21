b159792fc093686146423dbcb096f809be4d8c55
palm000palmovich <ivankiselev6534@gmail.com>
2025-04-21 22:27:38 +0300

    +constraints

--- src/main/java/ru/skypro/homework/dto/NewPassword.java
+++ src/main/java/ru/skypro/homework/dto/NewPassword.java
@@ -3,9 +3,13 @@ package ru.skypro.homework.dto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @Data
 public class NewPassword {
+    @Size(min = 8, max = 16, message = "От 8 до 16 символов")
     private String currentPassword;
+    @Size(min = 8, max = 16, message = "От 8 до 16 символов")
     private String newPassword;
 
     public NewPassword() {}
--- src/main/java/ru/skypro/homework/dto/User.java
+++ src/main/java/ru/skypro/homework/dto/User.java
@@ -12,9 +12,13 @@ import javax.validation.constraints.Size;
 @Data
 public class User {
     private Long id;
+    @Size(min = 4, max = 16, message = "От 4 до 16 символов")
     private String username;
+    @Size(min = 8, max = 16, message = "От 8 до 16 символов")
     private String password;
+    @Size(min = 3, max = 16, message = "От 3 до 16 символов")
     private String firstName;
+    @Size(min = 3, max = 16, message = "От 3 до 16 символов")
     private String lastName;
     @Pattern(regexp = "\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}",
             message = "Номер телефона должен соответствовать формату: +7 XXX XXX-XX-XX")
