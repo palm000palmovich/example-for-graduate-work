e8e79cd55577bfdd56be546eb6ef1f980a84e02f
IrinaSerebryakova <ia.serebr@gmail.com>
2025-04-26 17:25:21 +0300

    feat (@Mapper, @Entity Comment):
    Переименованы Comment -> CommentDTO, Comments -> CommentsDTO.
    Аннотация @Validated в контроллеры.
    Добавлены зависимости mapstruct, mapstruct-processor, lombok-mapstruct-binding, liquibase.
    Создана модель-сущность Comment в папке Model.
    Создан интерфейс CommentMapper для маппинга из DTO в Entity и обратно.
    Осуществлена миграция, создание таблицы comments.

--- src/main/java/ru/skypro/homework/repository/UserRepository.java
+++ src/main/java/ru/skypro/homework/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.skypro.homework.dto.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
